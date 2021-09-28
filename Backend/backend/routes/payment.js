75e481b64a68256116fb1b77290b59cd6033a22b
@ahmed.creativehub <ahmed.creativehub@gmail.com>
2021-09-28 20:30:34 +0530

    updated some files

--- Backend/backend/routes/payment.js
+++ Backend/backend/routes/payment.js
@@ -3,7 +3,46 @@ const router = require('express').Router()
 
 const Payment = require('../models/payment')
 
+//////ahmed/////////
+class APIfeatures {
+    constructor(query, queryString){
+        this.query = query;
+        this.queryString = queryString;
+    }
+    filtering(){
+        const queryObj = {...this.queryString}
+        const excludedFields = ['page', 'sort', 'limit']
+        excludedFields.forEach(el => delete(queryObj[el]))
 
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+        this.query.find(JSON.parse(queryStr))
+
+        return this;
+    }
+
+    sorting(){
+        if(this.queryString.sort){
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            this.query = this.query.sort(sortBy)
+        }else{
+            this.query = this.query.sort('-createdAt')
+        }
+
+        return this;
+    }
+
+    paginating(){
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 9
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit)
+        return this;
+    }
+}
+
+
+//////ahmed ///////////
 
 
 
@@ -23,14 +62,45 @@ router.post('/addPayment', async(req,res) =>{
 })
 
 
-//get all payments
-router.get('/getPayments',async (req,res) => {
-    try{
-        const payments = await Payment.find();
-        res.json(payments);
-    }catch (e) {
-        console.log(e)
+//get all payments saajid
+// router.get('/getPayments',async (req,res) => {
+//     try{
+//
+//
+//         const payments = await Payment.find();
+//         res.json(payments);
+//     }catch (e) {
+//         console.log(e)
+//     }
+// })
+
+
+///ahmed
+// retreiving  the product details from  product Document
+router.get('/getPayments',async(req, res) =>{
+
+    try {
+
+        console.log("inside paymnet")
+
+        const features = new APIfeatures(Payment.find(), req.query)
+            .filtering().sorting().paginating()
+
+        const products = await features.query
+
+        console.log(" paymnet issss : "+products.length)
+
+        res.json({
+            status: 'success',
+            result: products.length,
+            products: products
+        })
+
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
     }
+
+
 })
 
 //update
--- Frontend/client/src/components/viewPayment/viewPayment.js
+++ Frontend/client/src/components/viewPayment/viewPayment.js
@@ -1,256 +1,157 @@
-import React, { useRef, withRouter } from "react";
-import axios from "axios";
+import React, {useContext, useEffect, useRef, useState} from 'react'
+
+import Loading from '../utils/loading/Loading'
+import axios from 'axios'
+import Filters from '../products/Filters'
+import '../products/products.css';
+import {Link} from "react-router-dom";
 import { Table } from "react-bootstrap";
-import { useReactToPrint } from "react-to-print";
-import {Link} from 'react-router-dom';
+import ReactToPrint from "react-to-print";
+import Button from "@mui/material/Button";
+import SummarizeIcon from "@mui/icons-material/Summarize";
 
-const OrderHistory = () => {
+
+
+
+
+const ViewPayment = () => {
     const componentRef = useRef();
-    const handlePrint = useReactToPrint({
-        content: () => componentRef.current,
-    });
-
-    //
-    //
-
     return (
         <div>
-            <button
-                style={{
-                    marginLeft: 200,
-                    backgroundColor: "teal",
-                    width: 200,
-                    height: 40,
-                    color: "white",
-                    marginTop: 50,
-                }}
-                onClick={handlePrint}
-            >
-                Download Report
-            </button>
-            <ViewPayment ref={componentRef} />
+            <ReactToPrint
+
+                content={() => componentRef.current}
+                trigger={() =>
+                    // <Button className="reserve12"  style={{marginBottom:-200}}>Summary</Button>
+                    <div className='app2'>
+                        <div className="box2">
+                            <Button className="reserve13" endIcon={<SummarizeIcon />} style={{width:300,marginLeft:-970,backgroundColor:'teal'}} >Generate Report</Button>
+                        </div>
+                    </div>
+                }
+            />
+            <ComponentToPrint ref={componentRef} />
         </div>
     );
-};
-export default OrderHistory;
-
-class ViewPayment extends React.PureComponent {
-    constructor(props) {
-        super(props);
-    }
-
-    state = {
-        payments: [],
-
-    };
-
-
-    deleteUser(id){
-        axios.delete(`http://localhost:5000/payment/delete/${id}`)
-            .then(response => {
-                alert('Payment deleted')
-                window.location.reload(false);
-            })
-
-    }
+}
 
 
 
-    componentDidMount() {
-        axios
-            .get("http://localhost:5000/payment/getPayments")
-            .then((res) => {
-                //const payments = res.data.data;
-                console.log(res.data.data);
-                this.setState({ payments: res.data });
-                //console.log(products);
-                // this.setState({products:res.data.data});
-            })
-            .catch((err) => err.message);
-    }
-
-    render() {
-        return (
 
 
 
+
+
+
+const ComponentToPrint = React.forwardRef((props, ref) => {
+
+
+    // const [token] = state.token
+
+    const [loading, setLoading] = useState(false)
+    // const [search, setSearch] = state.productsAPI.search
+    // const [products, setProducts] = state.productsAPI.products
+    // const [callback, setCallback] = state.productsAPI.callback
+
+    const [payments, setPayments] = useState([])
+    const [page, setPage] = useState(1)
+    const [callback, setCallback] = useState(false)
+    const [search, setSearch] = useState('')
+
+
+    useEffect(() =>{
+        const getProducts = async () => {
+            const res = await axios.get(`http://localhost:5000/payment/getPayments?limit=${page*18}&cardName[regex]=${search}`)
+            setPayments(res.data.products)
+
+        }
+        getProducts()
+    },[callback, search, page])
+
+
+    if(loading) return <div><Loading /></div>
+    return (
+        <>
+            <div className="filter_menu">
+                <input type="text" value={search} placeholder="Enter your search!"
+                       onChange={e => setSearch(e.target.value.toLowerCase())} />
+            </div>
             <div className="container" style={{ paddingTop: "30px" }}>
                 {/* <h1>report</h1> */}
                 <p></p>
                 {/*<input label="Search Country" onChange={this.onChange}/>*/}
-                <Table
-                    striped
-                    bordered
-                    hover
-                    variant="dark"
-                    style={{
-                        marginLeft: 200,
-                        width: 900,
-                        backgroundColor: "#555555",
-                        border: 6,
-                    }}
-                >
-                    <thead>
-                    <tr>
-                        <th
-                            style={{
-                                color: "white",
-                                backgroundColor: "black",
-                                borderColor: "black",
-                            }}
-                        >
-                            Amount
-                        </th>
-                        <th
-                            style={{
-                                color: "white",
-                                backgroundColor: "black",
-                                borderColor: "black",
-                            }}
-                        >
-                            Card Type
-                        </th>
-                        <th
-                            style={{
-                                color: "white",
-                                backgroundColor: "black",
-                                borderColor: "black",
-                            }}
-                        >
-                            Card Number
-                        </th>
-                        <th
-                            style={{
-                                color: "white",
-                                backgroundColor: "black",
-                                borderColor: "black",
-                            }}
-                        >
-                            Card Name
-                        </th>
-                        <th
-                            style={{
-                                color: "white",
-                                backgroundColor: "black",
-                                borderColor: "black",
-                            }}
-                        >
-                            CVV
-                        </th>
-                        <th
-                            style={{
-                                color: "white",
-                                backgroundColor: "black",
-                                borderColor: "black",
-                            }}
-                        >
-                            Expiry Date
-                        </th>
+                <div  ref={ref}>
+                    <div className="history-page">
+
+                        <table  style={{
+                            backgroundColor: "#555555",
+                            width:1000,
+                            border: 6,
+                            marginTop:100
+                        }}>
+                            <thead>
+                            <tr>
+                                <th style={{ color: "white", backgroundColor: "black", borderColor: "black",}}>Amount</th>
+                                <th style={{ color: "white", backgroundColor: "black", borderColor: "black",}}>Card Type</th>
+                                <th style={{ color: "white", backgroundColor: "black", borderColor: "black",}}>Card Number</th>
+                                <th style={{ color: "white", backgroundColor: "black", borderColor: "black",}}>Card Name</th>
+                                <th style={{ color: "white", backgroundColor: "black", borderColor: "black",}}>CVV</th>
+                                <th style={{ color: "white", backgroundColor: "black", borderColor: "black",}}>Expire Date</th>
+                                <th style={{ color: "white", backgroundColor: "black", borderColor: "black",}}></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                payments.map(payments => (
 
 
+                                    <tr key={payments._id}>
+                                        <td  style={{ color: "white",backgroundColor: "#555555",borderColor: "#555555",}}>{payments.amount}</td>
+                                        <td  style={{ color: "white",backgroundColor: "#555555",borderColor: "#555555",}}>{payments.cardType}</td>
+                                        <td  style={{ color: "white",backgroundColor: "#555555",borderColor: "#555555",}}>{payments.cardNumber}</td>
+                                        <td  style={{ color: "white",backgroundColor: "#555555",borderColor: "#555555",}}>{payments.cardName}</td>
+                                        <td  style={{ color: "white",backgroundColor: "#555555",borderColor: "#555555",}}>{payments.cvv}</td>
+                                        <td  style={{ color: "white",backgroundColor: "#555555",borderColor: "#555555",}}>{payments.expiryDate}</td>
 
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.payments.map((payments) => {
-                        if (payments) {
-                            return (
-                                <tr>
-                                    <td
-                                        style={{
-                                            color: "white",
-                                            backgroundColor: "#555555",
-                                            borderColor: "#555555",
-                                        }}
-                                    >
-                                        {payments.amount}
-                                    </td>
-                                    <td
-                                        style={{
-                                            color: "white",
-                                            backgroundColor: "#555555",
-                                            borderColor: "#555555",
-                                        }}
-                                    >
-                                        {payments.cardType}
-                                    </td>
-                                    <td
-                                        style={{
-                                            color: "white",
-                                            backgroundColor: "#555555",
-                                            borderColor: "#555555",
-                                        }}
-                                    >
-                                        {payments.cardNumber}
-                                    </td>
-                                    <td
-                                        style={{
-                                            color: "white",
-                                            backgroundColor: "#555555",
-                                            borderColor: "#555555",
-                                        }}
-                                    >
-                                        {payments.cardName}
-                                    </td>
-                                    <td
-                                        style={{
-                                            color: "white",
-                                            backgroundColor: "#555555",
-                                            borderColor: "#555555",
-                                        }}
-                                    >
-                                        {payments.cvv}
-                                    </td>
-                                    <td
-                                        style={{
-                                            color: "white",
-                                            backgroundColor: "#555555",
-                                            borderColor: "#555555",
-                                        }}
-                                    >
-                                        {payments.expiryDate}
-                                    </td>
+                                        <td
+                                            style={{
+                                                color: "white",
+                                                backgroundColor: "#555555",
+                                                borderColor: "#555555",
+                                            }}
+                                        >
 
-                                    <td
-                                        style={{
-                                            color: "white",
-                                            backgroundColor: "#555555",
-                                            borderColor: "#555555",
-                                        }}
-                                    >
+                                            <Link to={`/getPaymentUpdate/${payments._id}`}>
+                                                <button  style={{
+                                                    backgroundColor: "Green",
+                                                    width: 50,
+                                                    height: 40,
+                                                    color: "white"
+                                                }}
+                                                >Update</button>
+                                            </Link>
 
-                                        <Link to={`/getPaymentUpdate/${payments._id}`}>
-                                            <button  style={{
-                                                backgroundColor: "Green",
+                                            <button style={{
+                                                // marginLeft: 200,
+                                                backgroundColor: "Red",
                                                 width: 50,
                                                 height: 40,
-                                                color: "white"
-                                            }}
-                                            >Update</button>
-                                        </Link>
+                                                color: "white",
 
-                                        <button style={{
-                                            // marginLeft: 200,
-                                            backgroundColor: "Red",
-                                            width: 50,
-                                            height: 40,
-                                            color: "white",
+                                            }}onClick={()=> this.deleteUser(payments._id)}
+                                            >Delete</button>
+                                        </td>
 
-                                        }}onClick={()=> this.deleteUser(payments._id)}
-                                        >Delete</button>
-                                    </td>
+                                    </tr>
 
-
-
-                                </tr>
-                            );
-                        }
-                    })}
-                    </tbody>
-                </Table>
+                                ))
+                            }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-        );
-    }
-}
-
-
+        </>
+    )
+})
+export default ViewPayment
